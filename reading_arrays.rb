f8c16a88da4e815d611e2e4de0986b7df87cb39e
AderonkeAdejare <aderonkea@comcast.net>
2018-07-25 15:20:25 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -40,8 +40,8 @@ last_student_by_method
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
   students = []
+  students << STUDENT_NAMES[0]
   students << STUDENT_NAMES[1]
   students << STUDENT_NAMES[2]
-  students << STUDENT_NAMES[3]
 end
 first_second_and_third_students
